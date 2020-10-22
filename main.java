b644c4a055996134bd4fac2f95567bd263ee47ea
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-10-22 11:40:35 -0500

    making sense of lested for loops_specifically to power integers

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+        
+        
+
+        toPowerExample example = new toPowerExample();
+
+        example.toPower(3,4);
+
+    }
+}
---
+++ toPowerExample.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/**
+ * toPower
+ */
+public class toPowerExample {
+
+    public static void toPower(int size, int power) {
+        
+        //creating a new array of ints, the size will be = size
+        //[0,1,2,3]
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+
+            newArray[i] = 1;
+
+            for (int j = 0; j < power; j++) {
+
+                newArray[i] *= i;
+
+                System.out.println(newArray[i]);
+            }
+        }
+
+        System.out.println(Arrays.toString(newArray));
+
+        
+
+
+    }
+
+
+}
